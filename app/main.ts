a188c050447ef7f9f2ba2251911b56b79c1e75c1
jaco <fbwotjq@gmail.com>
2016-09-02 01:38:43 +0900

    implement ngModel ..

--- app/main.ts
+++ app/main.ts
@@ -1,4 +1,5 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app.module';
+import { OtherAppModule } from './otherapp.module';
 platformBrowserDynamic().bootstrapModule(AppModule);
-
+platformBrowserDynamic().bootstrapModule(OtherAppModule);
